d1aea545b03a7d3554335f9a1c52a80241bd44ef
Taylor Keller <takeller17@gmail.com>
2020-04-17 17:30:24 -0600

    Add generate_keys test

--- test/encryption_algorithm_test.rb
+++ test/encryption_algorithm_test.rb
@@ -10,4 +10,17 @@ class EncryptionAlgorithmTest < MiniTest::Test
   def test_it_exists
     assert_instance_of EncryptionAlgorithm, @enigma_machine
   end
+
+  def test_it_can_generate_keys
+    # 02715
+    # return hash where a-d key points to correct digits
+
+    expected = {
+      a_key: 02,
+      b_key: 27,
+      c_key: 71,
+      d_key: 15
+    }
+    assert_equal expected, @enigma_machine.generate_keys 
+  end
 end
