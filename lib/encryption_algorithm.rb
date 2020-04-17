14506d87433cc54234d16ddee7c4b3bf4668f8d1
Taylor Keller <takeller17@gmail.com>
2020-04-17 17:23:58 -0600

    Add encryption algorithm superclass and test

---
+++ lib/encryption_algorithm.rb
@@ -0,0 +1,6 @@
+class EncryptionAlgorithm
+
+  def initialize(argument)
+    @argument = argument
+  end
+end
---
+++ test/encryption_algorithm_test.rb
@@ -0,0 +1,7 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/encryption_algorithm'
+
+class EncryptionAlgorithmTest < MiniTest::Test
+
+end
