28d8030e9a76bc47d5521ab2b5e9426adca10dd6
Taylor Keller <takeller17@gmail.com>
2020-04-17 17:17:51 -0600

    Add existence test

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -4,4 +4,11 @@ require './lib/enigma'
 
 class EnigmaTest < MiniTest::Test
 
+  def setup
+    @enigma = Enigma.new
+  end
+
+  def test_it_exists
+    assert_instance_of Enigma, @enigma 
+  end
 end
