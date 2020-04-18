b26cea017db63bcf0eacfd4c2c2d995c77582e52
Taylor Keller <takeller17@gmail.com>
2020-04-18 17:15:24 -0600

    Finish encrypt class

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -4,17 +4,26 @@ class Encrypt < EncryptionAlgorithm
 
 
   def encrypt_message
-    keys = generate_keys if @key == nil
+    keys = generate_keys if @encryption_key == nil
     offsets = generate_offsets if @date == nil
-    keys = format_keys(key) if @key != nil
+    keys = format_keys(@encryption_key) if @encryption_key != nil
     offsets = format_offsets(date) if @date != nil
-
+    # binding.pry
     final_shifts = calculate_shifts(keys, offsets)
     formated_message = format_message(@message)
     shifted_message = formated_message.map do |message_chunk|
       shift_chunk(message_chunk, final_shifts, :encrypt)
     end
-    shifted_message.flatten.join()
+    @message = shifted_message.flatten.join()
+    format_encryption_return
+  end
+
+  def format_encryption_return
+    {
+      encryption: @message,
+      key: @encryption_key,
+      date: @date
+    }
   end
 
 end
--- test/encrypt_test.rb
+++ test/encrypt_test.rb
@@ -17,11 +17,11 @@ class EncryptTest < MiniTest::Test
 
   def test_it_has_readable_attributes
     assert_equal "hello world", @encryptor.message
-    assert_equal "02715", @encryptor.key
+    assert_equal "02715", @encryptor.encryption_key
     assert_equal "040895", @encryptor.date
 
     assert_equal "hello world", @encryptor_defaults.message
-    assert_nil @encryptor_defaults.key
+    assert_nil @encryptor_defaults.encryption_key
     assert_nil @encryptor_defaults.date
   end
 
@@ -29,30 +29,33 @@ class EncryptTest < MiniTest::Test
     # keys a: 02, b: 27, c: 71, d: 15 || offsets a: 1, b: 0, c: 2, d: 5
     # Final shifts -> a: 3, b: 27, c: 73, d: 20
     # Given "hello world" expected "keder ohuluw"
-    keys = {
-      a_key: [0,2],
-      b_key: [2,7],
-      c_key: [7,1],
-      d_key: [1,5]
+    expected = {
+      encryption: "keder ohulw",
+      key: "02715",
+      date: "040895"
     }
-    offsets = {
-      a_offset: 1,
-      b_offset: 0,
-      c_offset: 2,
-      d_offset: 5
+
+    assert_equal expected, @encryptor.encrypt_message
+
+
+    @encryptor_defaults.set_key([0,2,7,1,5])
+    Date.stubs(:today).returns(Date.new(1995, 8, 4))
+    assert_equal expected, @encryptor_defaults.encrypt_message
+  end
+
+  def test_format_encryption_return
+    expected = {
+      encryption: "keder ohulw",
+      key: "02715",
+      date: "040895"
     }
-    @encryptor.stubs(:generate_keys).returns(keys)
-    @encryptor.stubs(:generate_offsets).returns(offsets)
+    Date.stubs(:today).returns(Date.new(1995, 8, 4))
 
-    assert_equal "keder ohulw", @encryptor.encrypt_message
+    @encryptor.encrypt_message
+    assert_equal expected, @encryptor.format_encryption_return
 
-    assert_equal "keder ohulw", @encryptor.encrypt_message
-
-    @encryptor_defaults.stubs(:generate_keys).returns(keys)
-    @encryptor_defaults.stubs(:generate_offsets).returns(offsets)
-
-    assert_equal "keder ohulw", @encryptor_defaults.encrypt_message
-
-    assert_equal "keder ohulw", @encryptor_defaults.encrypt_message
+    @encryptor_defaults.set_key([0,2,7,1,5])
+    @encryptor_defaults.encrypt_message
+    assert_equal expected, @encryptor_defaults.format_encryption_return
   end
 end
