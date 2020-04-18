74ab3fe4f94651adf2174c965c767b018fe98a9f
Taylor Keller <takeller17@gmail.com>
2020-04-18 15:26:42 -0600

    Add setup, existence, and readable attributes tests

--- test/encrypt_test.rb
+++ test/encrypt_test.rb
@@ -1,7 +1,27 @@
 require 'minitest/autorun'
 require 'minitest/pride'
+require 'mocha/minitest'
 require './lib/encrypt'
 
 class EncryptTest < MiniTest::Test
 
+  def setup
+    @encryptor = Encrypt.new("hello world", "02715", "040895")
+    @encryptor_defaults = Encrypt.new("hello world")
+  end
+
+  def test_it_exists
+    assert_instance_of Encrypt, @encryptor
+    assert_instance_of Encrypt, @encryptor_defaults
+  end
+
+  def test_it_has_readable_attributes
+    assert_equal "hello world", @encryptor.message
+    assert_equal "02715", @encryptor.key
+    assert_equal "040895", @encryptor.date
+
+    assert_equal "hello world", @encryptor_defaults.message
+    assert_equal "02715", @encryptor_defaults.key
+    assert_equal "040895", @encryptor_defaults.date
+  end
 end
