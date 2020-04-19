093ca87a30e21ea67fadcfdf181758cb8d471681
Taylor Keller <takeller17@gmail.com>
2020-04-19 14:54:51 -0600

    Add shift message functionality

--- lib/encryption.rb
+++ lib/encryption.rb
@@ -4,17 +4,8 @@ class Encryption < EncryptionAlgorithm
 
 
   def encrypt_message
-    # keys = generate_keys if @encryption_key == nil
-    # offsets = generate_offsets if @date == nil
-    # keys = format_keys(@encryption_key) if @encryption_key != nil
-    # offsets = format_offsets(date) if @date != nil
-    #
-    # final_shifts = calculate_shifts(keys, offsets)
-    # formated_message = format_message(@message)
     formated_input = format_encryption_input
-    shifted_message = formated_message.map do |message_chunk|
-      shift_chunk(message_chunk, final_shifts, :encrypt)
-    end
+    encrypted_message = shift_message(formated_input[:formated_message], formated_input[:shifts])
     @message = shifted_message.flatten.join()
     format_encryption_return
   end
@@ -31,6 +22,12 @@ class Encryption < EncryptionAlgorithm
       shifts: final_shifts,
       formated_message: formated_message,
     }
+  end
+
+  def shift_message(message,shifts)
+    message.map do |message_chunk|
+      shift_chunk(message_chunk, shifts, :encrypt)
+    end
   end
 
   def format_encryption_return
